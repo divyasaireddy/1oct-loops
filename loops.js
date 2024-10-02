aac3397b0f471e973dbc34c1eb41d3b4787bd936
divyasaireddy <divyasaireddy0910@gmail.com>
2024-10-03 00:01:31 +0530

    code of loops

---
+++ loops.js
@@ -0,0 +1,85 @@
+//1st que
+var day="saturday";
+
+switch(day){
+    case "monday":
+        console.log("monday");
+        break;
+    case "tuesday":
+        console.log("weekday");
+        break;
+    case"wednesday":
+        console.log("wednesday");
+        break;
+    case"thursday":
+        console.log("thursday");
+        break;
+    case "friday":
+        console.log("Friday");
+        break;
+    default:
+        console.log("weekend");       
+}
+
+//2nd que
+var month="December"
+
+switch(month){
+    case "January":
+        console.log("January");
+        break;
+    case "Febuary":
+        console.log("Febuary");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log( "april");
+        break;
+    case "may":
+        console.log( "may");
+        break;
+    case "june":
+        console.log("june");
+        break;
+    case "july":
+        console.log("july");
+        break;
+    case "august":
+        console.log("august");
+        break;
+    case "sep":
+        console.log("sep");
+        break;
+    case "oct":
+        console.log("oct");
+        break;
+    case "nov":
+        console.log("nov");
+        break;
+    default:
+        console.log("there is no month in the given list");
+        
+}
+
+//3rd que
+var a=1;
+while(a<=10){
+    if(a%2==0){
+        document.write(a + " ");
+    }
+    a++;
+}
+
+//4th que
+var a=10;
+while(a>=0){
+    console.log(a);
+    a--;
+   
+}
+
+
+
+
